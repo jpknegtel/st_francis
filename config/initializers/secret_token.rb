3223460d1f7ce00a7f11846c6be0ed9c64efb21a
Jonathan Knegtel <jpknegtel@gmail.com>
2012-01-18 18:21:59 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StFrancis::Application.config.secret_token = '473d3dc8400b72240cf8e003e0b94424244259b8d0501e6223c3d4679f08c2bde673cbd750a73348f88c40a2c5052da2421ddfecbc50393113adbd65a9d9e14e'
