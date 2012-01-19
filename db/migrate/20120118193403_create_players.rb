65555c66d67c887bd4233150ab9ea5ba002c407b
Jonathan Knegtel <jpknegtel@gmail.com>
2012-01-19 20:01:25 +0000

    Simple working to read

---
+++ db/migrate/20120118193403_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string "first_name"
+      t.string "last_name"
+      t.string "email"
+
+      t.timestamps
+    end
+  end
+end
