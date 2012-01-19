0ed37144ef58055d2993d19cca71bbe79edba222
Jonathan Knegtel <jpknegtel@gmail.com>
2012-01-19 21:26:38 +0000

    Simple working to read + create + edit

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,34 @@ class PlayersController < ApplicationController
   def list
     @players = Player.all
   end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(params[:player])
+    if @player.save
+      redirect_to(:action =>'list')
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+     @player = Player.find(params[:id])
+    if @player.update_attributes(params[:player])
+      redirect_to(:action =>'show', :id => @player.id)
+    else
+      render('edit')
+    end
+  end
 end
